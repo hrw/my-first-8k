c4e0024afb65f8fa51c56f53e67183a99095fe20
Marcin Juszkiewicz <marcin@juszkiewicz.com.pl>
2019-08-13 15:55:45 +0200

    titlescreen: z windy1

--- titlescreen/titlescreen_color.asm
+++ titlescreen/titlescreen_color.asm
@@ -1,10 +1,9 @@
 
- ; This is where the titlescreen background color gets set. 
- ; You can also do a "dim titlescreencolor=[letter]" in bB
+ ; This is where the titlescreen background color gets set.
+ ; You can also do a 'dim titlescreencolor=[letter]' in bB
  ; if you want to change the color on the fly.
 
  ifnconst titlescreencolor
 titlescreencolor
  endif
  .byte $00
-
